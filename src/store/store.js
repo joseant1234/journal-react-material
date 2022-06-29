6b47d06c09f0910591122af04be1a574c7a07c69
Jose <jose.antonio.umeres@gmail.com>
2022-06-29 14:51:02 -0500

    Added auth slice

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { authSlice } from './auth';
 
 // internamente el configureStore llama al combineReducers
 export const store = configureStore({
   reducer: {
-
+    auth: authSlice.reducer,
   },
 });
