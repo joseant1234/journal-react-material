f1fd90231f746bff21e70d14428880506ff5b34e
Jose <jose.antonio.umeres@gmail.com>
2022-07-01 21:12:49 -0500

    Added useForm to RegisterPage

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,25 @@
-import { Google } from "@mui/icons-material"
 import { Button, Grid, Link, TextField, Typography } from "@mui/material"
 import { Link as RouterLink } from 'react-router-dom';
+import { useForm } from "../../hooks/useForm";
 import { AuthLayout } from "../layout/AuthLayout";
 
+const formData = {
+  email: '',
+  password: '',
+  displayName: '',
+}
+
 export const RegisterPage = () => {
+
+  const { displayName, email, password, onInputChange } = useForm(formData);
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <AuthLayout title="Crear cuenta">
-      <form>
+      <form onSubmit={ onSubmit }>
         <Grid container>
           <Grid item xs={ 12 } sx={{ mt: 2 }}>
             <TextField
@@ -14,6 +27,9 @@ export const RegisterPage = () => {
               type="text"
               placeholder="Nombre"
               fullWidth
+              name="displayName"
+              value={displayName}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -23,6 +39,9 @@ export const RegisterPage = () => {
               type="email"
               placeholder="correo@google.com"
               fullWidth
+              name="email"
+              value={email}
+              onChange={onInputChange}
             />
           </Grid>
 
@@ -32,12 +51,19 @@ export const RegisterPage = () => {
               type="password"
               placeholder="Contraseña"
               fullWidth
+              name="password"
+              value={password}
+              onChange={onInputChange}
             />
           </Grid>
 
           <Grid container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
             <Grid item xs={ 12 }>
-              <Button variant='contained' fullWidth>
+              <Button
+                type="submit"
+                variant='contained'
+                fullWidth
+              >
                 Crear cuenta
               </Button>
             </Grid>
