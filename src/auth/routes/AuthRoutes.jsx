3ae39d2b13211edf761bc6518a5181e7e4df4a83
Jose <jose.antonio.umeres@gmail.com>
2022-07-05 15:45:49 -0500

    Added CheckingAuth component

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,7 +1,15 @@
+import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom'
+import { CheckingAuth } from '../../ui';
 import { LoginPage, RegisterPage } from '../pages';
 
 export const AuthRoutes = () => {
+  const { status } = useSelector(state => state.auth);
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
         <Route path="login" element={<LoginPage />} />
