77045c7db03849c7783cd713bb5f38a8335f0905
Jose <jose.antonio.umeres@gmail.com>
2022-07-05 21:12:11 -0500

    Kept state after refreshing the page

--- src/auth/routes/AuthRoutes.jsx
+++ src/auth/routes/AuthRoutes.jsx
@@ -1,14 +1,7 @@
-import { useSelector } from 'react-redux';
 import { Navigate, Route, Routes } from 'react-router-dom'
-import { CheckingAuth } from '../../ui';
 import { LoginPage, RegisterPage } from '../pages';
 
 export const AuthRoutes = () => {
-  const { status } = useSelector(state => state.auth);
-
-  if (status === 'checking') {
-    return <CheckingAuth />
-  }
 
   return (
     <Routes>
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,12 +1,42 @@
-import { Route, Routes } from "react-router-dom"
+import { onAuthStateChanged } from "firebase/auth";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom"
 import { AuthRoutes } from '../auth/routes/AuthRoutes';
+import { FirebaseAuth } from "../firebase/config";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
+import { login, logout } from "../store/auth/authSlice";
+import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
+
+  const { status } = useSelector(state => state.auth);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // cuando el estado de la autenticacion cambia
+    onAuthStateChanged(FirebaseAuth, async(user) => {
+      if(!user) return dispatch(logout());
+      const { uid, email, displayName, photoURL } = user;
+      dispatch(login({ uid, email, displayName, photoURL }));
+    });
+
+  }, [])
+
+
+  if (status === 'checking') {
+    return <CheckingAuth />
+  }
+
   return (
     <Routes>
-        <Route path="/auth/*" element={<AuthRoutes />} />
-        <Route path="/*" element={<JournalRoutes />} />
+        {
+          (status === 'authenticated')
+          ? <Route path="/*" element={<JournalRoutes />} />
+          : <Route path="/auth/*" element={<AuthRoutes />} />
+        }
+        {/* si ninguna ruta coincide va al login */}
+        <Route path='/*' element={<Navigate to='/auth/login' />} />
     </Routes>
   )
 }
