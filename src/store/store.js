963a8904109e6a5839a458d00d7b5525982d33e6
Jose <jose.antonio.umeres@gmail.com>
2022-07-06 20:40:11 -0500

    Added journal slices

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { authSlice } from './auth';
+import { journalSlice } from './journal/journalSlice';
 
 // internamente el configureStore llama al combineReducers
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
+    journal: journalSlice.reducer,
   },
 });
